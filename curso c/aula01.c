f068af04ae9cf9bcdd986fbc68ee288548ad42b2
cleannsz <zteuzynff@gmail.com>
2025-01-21 22:44:11 -0300

    Add files via upload

---
+++ curso c/aula01.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    int idade;
+    int quantidade = 1;
+    float altura = 1.78;
+    double peso = 77.98;
+    char letra = "A";
+    char nome[20] = "sergio";
+
+    idade = 10;
+    quantidade = 2;
+
+}
